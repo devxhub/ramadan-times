90256936af43591546447a97a2343a2f5cfd2b9e
git-shuvo1 <arfatul.shuvo@devxhub.com>
2024-12-05 17:14:10 +0600

    settings_page

---
+++ lib/src/settings/presentation/page/settings_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool showImsak = false;
+  bool autoDetectLocation = true;
+  bool automaticSettings = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Prayer Times'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          SwitchListTile(
+            title: Text('Show Imsak in Prayer Times page'),
+            value: showImsak,
+            onChanged: (value) {
+              setState(() {
+                showImsak = value;
+              });
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              'Prayer Time Calculation',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          ListTile(
+            title: Text('Location'),
+            subtitle: Text('-'),
+          ),
+          SwitchListTile(
+            title: Text('Auto-detect Location'),
+            value: autoDetectLocation,
+            onChanged: (value) {
+              setState(() {
+                autoDetectLocation = value;
+              });
+            },
+          ),
+          SwitchListTile(
+            title: Text('Automatic settings'),
+            value: automaticSettings,
+            onChanged: (value) {
+              setState(() {
+                automaticSettings = value;
+              });
+            },
+          ),
+          ListTile(
+            title: Text('Prayer time conventions'),
+            subtitle: Text('Muslim World League (MWL)\n(18.0° / 17.0°)'),
+          ),
+          ListTile(
+            title: Text('Manual corrections'),
+            subtitle: Text('0, 0, 0, 0, 0, 0'),
+          ),
+          ListTile(
+            title: Text('Asr calculation – Juristic method'),
+            subtitle: Text('Standard (Shafi, Maliki, Hanbali)'),
+          ),
+          ListTile(
+            title: Text('High Latitude Adjustment'),
+            subtitle: Text('Angle-based method'),
+          ),
+          ListTile(
+            title: Text('Daylight Saving Time'),
+            subtitle: Text('Auto'),
+          ),
+          ListTile(
+            title: Text('Imsak'),
+            subtitle: Text('0 minutes before Fajr'),
+          ),
+        ],
+      ),
+    );
+  }
+}
