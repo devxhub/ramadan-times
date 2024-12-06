7667102f57aa1860bcac65e04d48454a47813712
git-shuvo1 <arfatul.shuvo@devxhub.com>
2024-12-06 10:36:12 +0600

    settings with localization and responsiveness

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
