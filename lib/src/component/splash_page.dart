9238f9285f2502fa5a0e81d75f0776feed51b709
git-azmain <azmain.iqbal@devxhub.com>
2024-12-26 20:13:32 +0600

    splash design completed

--- lib/src/component/splash_page.dart
+++ lib/src/component/splash_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class SplashPage extends StatefulWidget {
-  const SplashPage({Key? key}) : super(key: key);
+  const SplashPage({super.key});
 
   @override
   State<SplashPage> createState() => _SplashPageState();
@@ -11,32 +16,162 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2), () {
-      context.go("/");
-    });
-
     super.initState();
+    Future.delayed(const Duration(seconds: 0), () {
+      _showBottomSheet();
+    });
   }
 
-  @override
-  didChangeDependencies() {
-    super.didChangeDependencies();
-
-    // _videoPlayerController.play().then((value) async {
-    //   // _videoPlayerController.pause();
-    //   await Future.delayed(const Duration(seconds: 4), () async {
-    //     context.go('/breathingPage');
-    //   });
-    //   _videoPlayerController.s
-    // });
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(15.sp)),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 24.h),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      context.go("/");
+                    },
+                    child: CircleAvatar(
+                      radius: 10.sp,
+                      backgroundColor: Colors.black,
+                      child: Icon(
+                        FontAwesomeIcons.x,
+                        size: 10.sp,
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Gap(16.h),
+              Text(
+                'Sign Up to',
+                style: GoogleFonts.dmSans(
+                  fontSize: 25.sp,
+                  fontWeight: FontWeight.w400,
+                  color: HexColor('#1F1947'),
+                ),
+              ),
+              Text(
+                'Muslim Times',
+                style: GoogleFonts.dmSans(
+                  fontSize: 24.sp,
+                  fontWeight: FontWeight.bold,
+                  color: HexColor('#1F1947'),
+                ),
+              ),
+              Gap(8.h),
+              Text(
+                'Muslim Times is a digital platform for all\nMuslims to follow prayer time',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.dmSans(
+                    fontSize: 16, color: HexColor("#1C1646")),
+              ),
+              const SizedBox(height: 24),
+              // Sign up With Google
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Image(image: AssetImage('assets/images/Google.png')),
+                    label: Text(
+                      'Sign Up with Google',
+                      style: GoogleFonts.dmSans(color: HexColor('#1C1646')),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Gap(12.h),
+              // Sign up With Apple
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.apple),
+                    label: Text('Sign Up with Apple'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Gap(12.h),
+              // Sign up With Email
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      'Sign Up with Email',
+                      style: GoogleFonts.dmSans(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: HexColor('#6348EB'),
+                    ),
+                  ),
+                ),
+              ),
+              Gap(12.h),
+              // Sign in Button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: GoogleFonts.dmSans(
+                      color: HexColor('#75718B'),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Sign in',
+                      style: GoogleFonts.dmSans(
+                        color: HexColor('#75718B'),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
       child: FittedBox(
-        // If your background video doesn't look right, try changing the BoxFit property.
-        // BoxFit.fill created the look I was going for.
         fit: BoxFit.fill,
         child: Image.asset("assets/images/Splash.png"),
       ),
