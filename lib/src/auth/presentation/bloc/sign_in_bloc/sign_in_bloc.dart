a7ea5915067267f17ce35a095809ecb7a89d3a13
Eliyas Hossain <eliyas.ru1991@gmail.com>
2024-12-31 22:17:04 +0600

    feat: change app name

--- lib/src/auth/presentation/bloc/sign_in_bloc/sign_in_bloc.dart
+++ lib/src/auth/presentation/bloc/sign_in_bloc/sign_in_bloc.dart
@@ -3,12 +3,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../../../services/constants.dart';
-import '../../../data/models/sign_in_response.dart';
-import '../../../data/respositories/sign_in_repository.dart';
+import 'package:muslimtimespro/src/auth/data/respositories/sign_in_repository.dart';
+import 'package:muslimtimespro/src/models/address/district.dart';
+import 'package:muslimtimespro/src/prayer_times/data/models/country_response.dart';
+import 'package:muslimtimespro/src/prayer_times/data/models/manual_prayer_time.dart';
+import 'package:muslimtimespro/src/prayer_times/data/models/prayer_times.dart';
+import 'package:muslimtimespro/src/prayer_times/data/models/user_coordinates.dart';
+import 'package:muslimtimespro/src/prayer_times/data/models/weather_model.dart';
+import 'package:muslimtimespro/src/prayer_times/data/repositories/prayer_time_repository.dart';
 
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
